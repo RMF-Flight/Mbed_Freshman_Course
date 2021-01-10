433cc7725483d3913fd4d49bbecb6eeec2f56fae
Kota Ishizeki <65107157+Kota0217@users.noreply.github.com>
2021-01-10 16:27:33 +0900

    Create main.cpp
    firts commit

---
+++ mbed_TWELITE_serial/main.cpp
@@ -0,0 +1,21 @@
+#include "mbed.h"
+
+Serial twe(PA_9, PA_10);
+int i = 0;
+
+//整数を1つ入力して、それをTWELITEで送る関数
+void tweTransmit(int i){
+        twe.printf("%d\n", i);
+}
+
+int main(){
+    twe.baud(115200);   //TWELITEとNUCLEOの通信のbaudrateを115200bpsに設定
+    twe.printf("Start!\n\n");
+    
+    while(1){
+        tweTransmit(i);
+        i++;
+        wait(1.0);
+    }
+    
+}
